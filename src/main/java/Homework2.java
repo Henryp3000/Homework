3126ee80a43c858bd98a9aceb8cf236b889f10f8
your name <Your email>
2022-08-31 19:14:52 -0400

    starting homework

---
+++ src/main/java/Homework2.java
@@ -0,0 +1,13 @@
+public class Homework2 {
+
+    public static void main(String[] args) {
+        // Java out put
+        System.out.println("This is my Homework");
+
+//         print variables
+        System.out.println();
+    }
+
+
+}
+
