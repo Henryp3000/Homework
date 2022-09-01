8d4c99a12b5544a727477ebc53734a51a50029fb
your name <Your email>
2022-08-31 21:15:26 -0400

    hey

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -1,13 +1,66 @@
+import java.sql.SQLOutput;
+
 public class Homework2 {
 
     public static void main(String[] args) {
         // Java out put
+//        create examples
+
         System.out.println("This is my Homework");
-
-//         print variables
         System.out.println("Hi");
-    }
+        System.out.println("I will Not Give up");
 
+        //         print variables
+//        String stores text "Hello" "Hi"
+//        int stores whole number 12345 or 54321
+//        float stores decimals 5.99 9.99
+//        char stores single characters (Y or N) (M or W)
+//        boolean stores statement (Yes or no) (ture or false)
+//        declaring or creating a variable assign a value = Henry
+        String name = "Henry";
+        System.out.println("name");
+
+        int myAge = 20;
+        System.out.println(myAge);
+
+        myAge = 23; //myAge is now age 23
+        System.out.println(myAge);
+
+
+        String student1_Name = "Henry";
+        String student1_age = "29";
+        char student1_sex = 'M';
+
+
+
+        student1_Name = "Henry";
+        System.out.println("Name: Henry");
+
+        student1_age = "29";
+
+        System.out.println("Age:29");
+
+
+
+        student1_sex = 'M';
+
+        System.out.println("sex: M");
+
+
+
+        //Step 1 - Create main class
+        //Nameofclassyourmade + variableName = new + nameofmethod()
+        //EX Car variable1 = new dreamCar();
+        Car variable1 = new Car();
+        //Step 2 - Identify attributes and give them values
+        variable1.minSpeed = 100;
+        variable1.maxSpeed = 500;
+
+
+
+
+    }
+// Data Types
 
 }
 
